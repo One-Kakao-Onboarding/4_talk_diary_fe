c828d382fdb4494c1d8a6fa012db243392bdbf93
unknown <kimsh4265@korea.ac.kr>
2026-01-15 10:42:55 +0900

    deploy: github page 배포 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/4_talk_diary_fe" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
